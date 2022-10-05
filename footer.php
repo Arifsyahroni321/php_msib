c2c7fd795a08a793b02d22e96c38d6bb3fc56753
MOH. ARIF SYAHRONI <98003541+Arifsyahroni321@users.noreply.github.com>
2022-10-05 15:56:55 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<section class="footer-container">
+        <div class="container">
+            <div class="row footer-containertent">
+                <div class="col-md-8">
+                    <h4>Tujuan kebijakan Merdeka Belajar - </h4>
+                    <p>Kampus Merdeka, program “hak belajar tiga semester di luar program studi” adalah untuk meningkatkan kompetensi lulusan, baik soft skills maupun hard skills, agar lebih siap dan relevan dengan kebutuhan zaman, menyiapkan lulusan sebagai pemimpin masa depan bangsa yang unggul dan berkepribadian. Program-program experiential learning dengan jalur yang fleksibel diharapkan akan dapat memfasilitasi mahasiswa mengembangkan potensinya sesuai dengan passion dan bakatnya.</p>
+                </div>
+                <div class="col-md-4 contac-us">
+                    <h4>Contact Us</h4>
+                    <p>Hubungi saya jika anda membutuhkan bantuan.</p>
+                    <ul>
+                        <li><i class="fa fa-home"></i>jln. pakuniran-paiton</li>
+                        <li><i class="fa fa-phone"></i>082 247 169 521</li>
+                        <li><i class="fa fa-envelope-o"></i>syahroniluffy321@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
