d508a5747a8c09f39d398415a81d2e15781ac98e
MOH. ARIF SYAHRONI <98003541+Arifsyahroni321@users.noreply.github.com>
2022-10-05 16:10:10 +0700

    Add files via upload

---
+++ myporto_msib_php/about.php
@@ -0,0 +1,49 @@
+<section id="about" class="about-sec section-wrapper description">
+        <div class="container">
+            <div class="row">
+                <!-- Section Header -->
+                <div class="col-md-12 col-sm-12 col-xs-12 section-header wow fadeInDown">
+                    <h2><span class="highlight-text">About Me</span></h2>
+
+                    <p class="col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">Halo semuanya saya adalah seorang perlajar dari Universitas Nurul Jadid Paiton-Probolinggo-JawaTimur, Dan saya merupakan Web dev junior yang dalam masa belajar. </p>
+                </div>
+                <!-- Section Header End -->
+
+                <div class="col-md-6 col-sm-6 col-xs-12 custom-sec-img wow fadeInDown">
+                    <img src="images/about.jpg" alt="Custom Image">
+                </div>
+
+                <div class="col-md-6 col-sm-6 col-xs-12 customized-text wow fadeInDown black-ed">
+                    <p>Saya adalah seorang mahasiswa bernama MOH. ARIF SYAHRONI, prodi saya ialah Tekhnik Informatika dan sekarank berada di Semester 5. saya merupakan anak ke 2 dari 1 saudara.</p>
+                    <div class="row">
+                        <div class="col-md-11">
+                            <strong>Data Pribadi:</strong>
+                            <br>Nama : MOH. ARIF SYAHRONI
+                            <BR/>ALAMAT: Probolinggo-jawatimur
+                            <BR/>Tempt tgl lahir: Probolinggo, 28-09-2002
+                            <br/>AGAMA : ISLAM
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-11">
+                            <strong>Visi & Misi</strong>
+                            <p><b>Visi</b> Menjadi guru IT yang terampil di bidangnya serta menggunakan metode yang menyenangkan dan membuat siswa dapat Tau tentan IT.
+                            </p><b>Misi</b>
+                            <ol>
+                                <li>menerpakan ilmu IT di setiap sekolah agar terjaringnya informasi di berbagai plosok</li>
+                                <li>Mengikuti Era 5.0  yang serba Teknologi</li>
+                            </ol>
+                            
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-11">
+                            <strong>SKILL</strong>
+                            <p><b>HTML, CSS, Javasript</b>mampu membuat tampilan web dengan responsive menggunakan framework Bootstrap 5.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
